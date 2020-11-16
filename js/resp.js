cbe07c9dd639054ac68f3593fa02019593970f39
ChitrikaGahtori <gahtorichitrika7272@gmail.com>
2020-11-17 00:16:45 +0530

    responsive_website

---
+++ js/resp.js
@@ -0,0 +1,10 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navList = document.querySelector('.navlist');
+rightNav = document.querySelector('.rightnav');
+
+burger.addEventListener('click' , ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
